21f631fcc0dc0c529e9f201ca20f7be404d6fb76
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-10-15 21:19:55 -0700

    Component, pageProps, nav bar and footer wrappers

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import { ThemeProvider } from 'next-themes';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import '@/styles/globals.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <h1>
+        NAVBAR
+      </h1>
+      <Component {...pageProps} />
+      <h1>
+        FOOTER
+      </h1>
+    </div>
+  </ThemeProvider>
+);
+
+export default MyApp;
