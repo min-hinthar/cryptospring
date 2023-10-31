f0536eb5a08a076b7424689b6ba3138b20c5b761
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-10-31 12:02:26 -0700

    Banner.jsx created to render as a component in _app.js through index.js, mobile responsive banner

--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ import Navbar from './Navbar';
 // eslint-disable-next-line import/no-cycle
 import Footer from './Footer';
 import Button from './Button';
+import Banner from './Banner';
 
-export { Navbar, Footer, Button };
+export { Navbar, Footer, Button, Banner };
