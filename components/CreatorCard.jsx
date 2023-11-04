fcee186b5832ef4b57122c043c1a248c0e142ad7
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-11-04 13:09:18 -0700

    CreatorCard.jsx updated to render creatorImage, green tick and rank

--- components/CreatorCard.jsx
+++ components/CreatorCard.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import Image from 'next/image';
 
-const CreatorCard = () => (
-  <div>CreatorCard</div>
+import images from '../assets';
+
+const CreatorCard = ({ rank, creatorImage, creatorName, creatorEths }) => (
+  <div className="min-w-190 minlg:min-w-240 dark:bg-nft-black-3 bg-white border dark:border-nft-black-1 border-nft-gray-1 rounded-3xl flex flex-col p-4 m-4">
+    <div
+      className="w-8 h-8 minlg:w-10 minlg:h-10 bg-nft-red-violet flexCenter rounded-full"
+    >
+      <p className="font-poppins text-white font-semibold text-base minlg:text-lg">
+        {rank}
+      </p>
+    </div>
+
+    <div className="my-2 flex justify-center">
+      <div className="relative w-20 h-20 minlg:w-28 minlg:h-28">
+        <Image
+          src={creatorImage}
+          layout="fill"
+          objectFit="cover"
+          alt="creator"
+          className="rounded-full"
+        />
+        <div
+          className="absolute w-4 h-4 minlg:w-7 minlg:h-7 bottom-2 -right-0"
+        >
+          <Image
+            src={images.tick}
+            layout="fill"
+            objectFit="contain"
+            alt="tick"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default CreatorCard;
