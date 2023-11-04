784857aad9ff69c5bddd04e2f737437de27742d4
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-11-04 16:28:35 -0700

    handleScroll left right arrow setHideButton if current scrollWidth >= parent offsetWidth

--- components/CreatorCard.jsx
+++ components/CreatorCard.jsx
@@ -34,6 +34,15 @@ const CreatorCard = ({ rank, creatorImage, creatorName, creatorEths }) => (
         </div>
       </div>
     </div>
+
+    <div className="mt-3 minlg:mt-7 text-center flexCenter flex-col">
+      <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base">
+        {creatorName}
+      </p>
+      <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base">
+        {creatorEths.toFixed(2)} <span> ETH </span>
+      </p>
+    </div>
   </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
 import { Banner, CreatorCard } from '../components';
 
@@ -6,8 +8,42 @@ import images from '../assets';
 import { makeId } from '../utils/makeId';
 
 const Home = () => {
+  const [hideButton, setHideButton] = useState(false);
+  const theme = useTheme();
   const parentRef = useRef(null);
   const scrollRef = useRef(null);
+
+  const handleScroll = (direction) => {
+    const { current } = scrollRef;
+
+    const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
+
+    if (direction === 'left') {
+      current.scrollLeft -= scrollAmount;
+    } else {
+      current.scrollLeft += scrollAmount;
+    }
+  };
+
+  const isScrollable = () => {
+    const { current } = scrollRef;
+    const { current: parent } = parentRef;
+
+    if (current?.scrollWidth >= parent?.offsetWidth) {
+      setHideButton(false);
+    } else {
+      setHideButton(true);
+    }
+  };
+  useEffect(() => {
+    isScrollable();
+
+    window.addEventListener('resize', isScrollable);
+
+    return () => {
+      window.removeEventListener('resize', isScrollable);
+    };
+  });
 
   return (
 
@@ -27,20 +63,49 @@ const Home = () => {
           <div
             className="relative flex-1 max-w-full flex mt-3"
             ref={parentRef}
-          />
-          <div
-            className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
-            ref={scrollRef}
           >
-            {[6, 7, 8, 9, 10].map((i) => (
-              <CreatorCard
-                key={`creator-${i}`}
-                rank={i}
-                creatorImage={images[`creator${i}`]}
-                creatorName={`0x${makeId(3)}...${makeId(4)}`}
-                creatorEths={10 - i * 0.5}
-              />
-            ))}
+            <div
+              className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
+              ref={scrollRef}
+            >
+              {[6, 7, 8, 9, 10].map((i) => (
+                <CreatorCard
+                  key={`creator-${i}`}
+                  rank={i}
+                  creatorImage={images[`creator${i}`]}
+                  creatorName={`0x${makeId(3)}...${makeId(4)}`}
+                  creatorEths={10 - i * 0.5}
+                />
+              ))}
+              {!hideButton && (
+                <>
+                  <div
+                    onClick={() => handleScroll('left')}
+                    className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0"
+                  >
+                    <Image
+                      src={images.left}
+                      layout="fill"
+                      objectFit="contain"
+                      alt="left_arrow"
+                      className={theme === 'light' && 'filter invert'}
+                    />
+                  </div>
+                  <div
+                    onClick={() => handleScroll('right')}
+                    className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0"
+                  >
+                    <Image
+                      src={images.right}
+                      layout="fill"
+                      objectFit="contain"
+                      alt="right_arrow"
+                      className={theme === 'light' && 'filter invert'}
+                    />
+                  </div>
+                </>
+              )}
+            </div>
           </div>
         </div>
       </div>
