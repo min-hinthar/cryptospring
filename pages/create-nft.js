a0ba51310b273a35cb2c74a40d36aef665dd8d40
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-05 15:09:23 -0800

    create-nft.js updated with imports useDropzone, useCallback, useMemo, useState, useContext, useRouter, onDrop, accept, maxSize

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import { useState, useMemo, useCallback, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useDropzone } from 'react-dropzone';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
-const CreateNFT = () => (
-  <div>CreateNFT</div>
-);
+import { Button } from '../components';
+import images from '../assets';
+
+const CreateNFT = () => {
+  const [fileUrl, setFileUrl] = useState(null);
+  const { theme } = useTheme();
+
+  const onDrop = useCallback(() => {
+    // upload image to the ipfs blockchain
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({
+    onDrop,
+    accept: 'image/*',
+    maxSize: 5000000,
+  });
+
+  const fileStyle = useMemo(() => (
+    `dark:bg-nft-black-1 bg-white border dark:border-white border-nft-gray-2 flex flex-col items-center p-5 rounded-sm border-dashed
+    ${isDragActive && 'border-file-active'}
+    ${isDragAccept && 'border-file-accept'}
+    ${isDragReject && 'border-file-reject'}
+    `
+  ), [isDragActive, isDragAccept, isDragReject]);
+
+  return (
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-3/5 md:w-full">
+        <h1
+          className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl text-semibold ml-4 xs:ml-0"
+        >
+          Create New NFT
+        </h1>
+        <div className="mt-16">
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">
+            Upload File
+          </p>
+          <div className="mt-4">
+            <div {...getRootProps()} className={fileStyle}>
+              <input {...getInputProps()} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CreateNFT;
