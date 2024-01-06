3aaabe3b3d797825c0f2f38a8f4822e8ef9365be
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-05 22:04:58 -0800

    Fetch Market Items function and Fetch My NFTs functions implemented

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -143,4 +143,54 @@ contract NFTMarketplace is ERC721URIStorage {
         payable(owner).transfer(listingPrice);
         payable(idToMarketItem[tokenId].seller).transfer(msg.value);
     }
+
+    // Fetch Market Items /* View Only
+    function fetchMarketItems() public view returns (MarketItem[] memory) {
+        uint itemCount = _tokenIds.current();
+        uint unsoldItemCount = _itemsSold.current() - _itemsSold.current();
+        uint currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItemCount);
+
+        for(uint i = 0; i < itemCount; i++ ) {
+            if(idToMarketItem[i + ].owner == address(this)) {
+                uint currentId = i + 1;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+
+                items[currentIndex] = currentItem;
+
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+    // Fetch MY NFTs
+    function fetchMyNFTs() public view returns (MarketItem[] memory) {
+        uint totalItemCount = _tokenIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for(uint i = 0; i < totalItemCount; i++ ) {
+            if(idToMarketItem[i + 1].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+        
+        MarketItem[] memory items = new MarketItem[](itemCount);
+
+        for(uint i = 0; i < itemCount; i++ ) {
+            if(idToMarketItem[i+1].owner == msg.sender) {
+                uint currentId = i + 1;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+
+                items[currentIndex] = currentItem;
+
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
 }
