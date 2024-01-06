13916fc40c20db84421dbbd37c7690f75da10bcf
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-05 18:18:49 -0800

    function createToken _mint, _setTokenURI, return newTokenId

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -70,4 +70,18 @@ contract NFTMarketplace is ERC721URIStorage {
     function getListingPrice() public view returns (uint256) {
         return listingPrice;
     }
+
+    // Create New Token
+    function createToken(string memory tokenURI, uint256 price) public payable returns (uint) {
+        _tokenIds.increment();
+
+        uint256 newTokenId = _tokenIds.current();
+
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        createMarketItem(newTokenId, price);
+
+        return newTokenId;
+    }
 }
