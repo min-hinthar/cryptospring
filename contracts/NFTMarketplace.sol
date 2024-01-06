c0d29c2b205c6db8ba733dcf912e227f0c042092
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-05 18:53:10 -0800

    Resell Token function implemented

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -84,4 +84,46 @@ contract NFTMarketplace is ERC721URIStorage {
 
         return newTokenId;
     }
+
+    // Create Market Item for New Token
+    function createMarketItem(uint256 tokenId, uint256 price) private {
+        // require a certain CONDITION, in this case price greater than 0
+        require(price > 0, "Price must be at least 1");
+        // require that the users sending in the transaction is sending in the correct amount
+        require(msg.value == listingPrice, "Price must be equal to listing price");
+
+        // create the mapping for the market items 
+        // payable(address(0)) is the owner. 
+        // currently there's no owner as the seller is putting it to market so it's an empty address
+        // last value  is boolean for sold, its false because we just put it so it's not sold yet
+        // this is creating the first market item
+        idToMarketItem[tokenId] = MarketItem(
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price,
+            false
+        );
+
+    // Transfer Ownership of Token from Sender to Address(this), tokenId
+        _transfer(msg.sender, address(this), tokenId);
+
+        emit MarketItemCreated(tokenId, msg.sender, address(this), price, false); 
+    }
+    
+    // Resell Token
+    function resellToken(uint256 tokenId, uint256 price) public payable {
+        require(idToMarketItem[tokenId].owner == msg.sender, "Only item owner can perform this operation!");
+        require(msg.value == listingPrice, "Price must be equal to listing price!");
+
+        idToMarketItem[tokenId].sold = false;
+        idToMarketItem[tokenId].price = price;
+        idToMarketItem[tokenId].seller = payable(msg.sender);
+        idToMarketItem[tokenId].owner = payable(address(this));
+
+        _itemsSold.decrement();
+
+        _transfer(msg.sender, address(this), tokenId);
+
+    }
 }
