ffec9388a43bad90ee4dae1a14157da8b86bddfb
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-06 21:14:13 -0800

    NFTContext.js created, useContext React.createContext(), nftCurrency changed from ETH to MATIC

---
+++ context/NFTContext.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import Web3Modal from 'web3modal';
+import { ethers } from 'ethers';
+import axios from 'axios';
+
+export const NFTContext = React.createContext();
+
+export const NFTProvider = ({ children }) => {
+  const nftCurrency = 'MATIC';
+
+  return (
+    <NFTContext.Provider value={{ nftCurrency }}>
+      {children}
+    </NFTContext.Provider>
+  );
+};
