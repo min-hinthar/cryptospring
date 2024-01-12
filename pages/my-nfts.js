25be621e93de29978b308548b260c2a77f5957a1
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-01-11 16:02:56 -0800

    nft-details.js updated with useRouter to check CurrentAccount === nft.seller render Buy Button with price and nftCurrency

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -5,7 +5,7 @@ import { NFTContext } from '../context/NFTContext';
 import { Loader, NFTCard, Banner } from '../components';
 
 import images from '../assets';
-import { shortenAddress } from '@/utils/shortenAddress';
+import { shortenAddress } from '../utils/shortenAddress';
 
 const MyNFTs = () => {
   const { fetchMyNFTsOrListedNFTs, currentAccount } = useContext(NFTContext);
